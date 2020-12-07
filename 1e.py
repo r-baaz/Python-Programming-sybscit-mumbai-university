22f622fdb80ebb6928ee365594b43ab990b30211
r-baaz <58431272+r-baaz@users.noreply.github.com>
2020-12-08 00:58:13 +0530

    Update 1e.py

--- 1e.py
+++ 1e.py
@@ -1,26 +1,24 @@
-def Armstrong_no(x):
-	no=x 
+def Armstrong_no(n):
 	sum=0
-	while no>0: 
-	   R=no%10
-	   sum=sum+(R*R*R)
-	   no=no//10
-	if num==sum:
+	while n>0: 
+	   r=n%10
+	   sum=sum+(r*r*r)
+	   n=n//10
+	if (num==sum):
 	   print(num,"is an Armstrong number")
 	else:
 	   print(num,"is not an Armstrong number")
 	   
-def Palindrome(Number):
-        n=Number
-        Reverse=0
-        while n>0:
-            Reminder=n%10
-            Reverse=(Reverse*10)+Reminder
-            n=n//10
-        if(Number==Reverse):
-                print(Number,'is a palindrome number')
+def Palindrome(n):
+        rev=0
+        while(n>0):
+                rem=n%10
+                rev=(rev*10)+rem
+                n=n//10
+        if(num==rev):
+                print(num,'is a palindrome number')
         else:
-                print(Number,'is not a palindrome number')
+                print(num,'is not a palindrome number')
 
 num=int(input("Enter a number: "))
 Armstrong_no(num)
